f6bfbe53340b9032104017055bcfb89f877b31e7
BK <benkoshy@hotmail.com>
2023-12-06 07:15:53 +1100

    WIP: json-api experiment

--- test/pagy/extras/json_api_test.rb
+++ test/pagy/extras/json_api_test.rb
@@ -64,7 +64,7 @@ describe 'pagy/extras/metadata' do
 
     def links
      	{ 	"self": "http://example.com/articles?page[number]=3&page[size]=1", 
-     		"first": "http://example.com/articles?page[number]=1&page[size]=1",
+     	  	"first": "http://example.com/articles?page[number]=1&page[size]=1",
 	        "prev": "http://example.com/articles?page[number]=2&page[size]=1",
 	        "next": "http://example.com/articles?page[number]=4&page[size]=1",
 	        "last": "http://example.com/articles?page[number]=13&page[size]=1"
