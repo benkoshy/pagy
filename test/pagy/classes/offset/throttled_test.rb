89865d6e9fea97cde4318e3558db62433a192623
Ben Koshy <benkoshy@hotmail.com>
2025-11-25 23:14:48 +1100

    WIP: throttled - test initialization

--- test/pagy/classes/offset/throttled_test.rb
+++ test/pagy/classes/offset/throttled_test.rb
@@ -1,1 +1,10 @@
-throttled_test.rb
+require_relative '../../../test_helper'
+require_relative '../../../../gem/lib/pagy/toolbox/helpers/support/series' # just to check the series
+
+describe 'pagy throttled' do
+	let(:pagy) { Pagy::Offset::Throttled.new(count: 100, page: 4) }
+
+	it "initializes" do
+		_(pagy).must_be_instance_of Pagy::Offset::Throttled
+	end
+end
