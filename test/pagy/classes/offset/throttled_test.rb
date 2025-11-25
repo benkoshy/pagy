ea5edc6d7ea72074729017cf4c4ea16563e2ec59
Ben Koshy <benkoshy@hotmail.com>
2025-11-25 23:59:17 +1100

    WIP: throttled class - test protected method

--- test/pagy/classes/offset/throttled_test.rb
+++ test/pagy/classes/offset/throttled_test.rb
@@ -7,4 +7,12 @@ describe 'pagy throttled' do
 	it "initializes" do
 		_(pagy).must_be_instance_of Pagy::Offset::Throttled
 	end
+
+	it "initializes pages" do 
+		 Pagy::Offset::Throttled.new(count: 100, page: 1)
+		_(pagy.send(:compose_page_param, 1)).must_equal "1+100+"
+	end
+
+	
+
 end
